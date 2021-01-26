e24034379abef73d5707a7a48b396a5772cf4ddd
Hong Xu <hongxu@cuhk.edu.hk>
2021-01-26 15:58:28 +0800

    .sh file added

---
+++ files/cgi-bin/hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Content-type: text/html"
+echo ""
+
+echo '<html>'
+echo '<head>'
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
+echo '<title>Hello World</title>'
+echo '</head>'
+echo '<body>'
+echo 'Hello World'
+echo '</body>'
+echo '</html>'
