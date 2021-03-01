4cde426a06bf423ef31ca15d93be6a3d251e7b1d
Hong Xu <hongxu@cuhk.edu.hk>
2021-03-01 20:50:38 +0800

    py files

---
+++ files/python/print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# for python 2
+# print "Double\tquotes"
+# print 'Double\tquotes'
+# print r'Raw\tstring'
+
+# for python 3
+print("Double\tquotes")
+print('Double\tquotes')
+print(r'Raw\tstring')
---
+++ files/python/types.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+print(int(1.6))
+print(int("FF", base=16))
+# FF = 11111111 base2
+print(float("3.8"))
+
+print(long("3"))
